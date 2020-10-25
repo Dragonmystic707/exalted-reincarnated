defa1a9b094ad6ab0a9791b1099d82a763540358
Dragonmystic <dragonmystic707@gmail.com>
2020-10-25 18:52:16 -0500

    Transition CSS

--- docs/public/js/script.js
+++ docs/public/js/script.js
@@ -2,13 +2,23 @@
   var toggle = document.querySelector('.sidebar-toggle');
   var sidebar = document.querySelector('#sidebar');
   var checkbox = document.querySelector('#sidebar-checkbox');
+  var container = document.querySelector('.container');
 
-  // document.addEventListener('click', function(e) {
-  //   var target = e.target;
+  document.addEventListener('click', function(e) {
+    var target = e.target;
 
-  //   if(!checkbox.checked ||
-  //      sidebar.contains(target) ||
-  //      (target === checkbox || target === toggle)) return;
-
-  // }, false);
+    // /* There's got to be a better way of doing this */
+    // if(target === checkbox || target === toggle)
+    // {
+    //   if(checkbox.checked)
+    //   {
+    //     container.padding.right = '15rem';
+    //   }
+    //   else
+    //   {
+    //     container.padding.right = '1rem'; 
+    //   }
+      
+    // }
+  }, false);
 })(document);
