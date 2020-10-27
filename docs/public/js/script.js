1083d53713bd56187bf98070d0b01fc5fa89f33d
Dragonmystic <dragonmystic707@gmail.com>
2020-10-26 22:53:46 -0500

    Fixed reference

--- docs/public/js/script.js
+++ docs/public/js/script.js
@@ -1,6 +1,6 @@
 (function(document) {
   const content = document.querySelector('.page');
-  var activeSidebarItem = document.querySelector('.sidebar-bottom');
+  var activeSidebarItem = document.querySelector('.sidebar-split');
 
   scrollnav.init(content, { 
     debug: false,
