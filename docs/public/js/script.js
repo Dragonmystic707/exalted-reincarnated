a41ef9b727a4d4b23b4394dbb340b05b92078a64
Dragonmystic <dragonmystic707@gmail.com>
2020-10-26 22:20:19 -0500

    Change sidebar nav query

--- docs/public/js/script.js
+++ docs/public/js/script.js
@@ -1,6 +1,6 @@
 (function(document) {
   const content = document.querySelector('.page');
-  var activeSidebarItem = document.querySelector('.sidebar-nav-item active');
+  var activeSidebarItem = document.querySelector('a.sidebar-nav-item' > 'a.active');
 
   scrollnav.init(content, { 
     debug: false,
