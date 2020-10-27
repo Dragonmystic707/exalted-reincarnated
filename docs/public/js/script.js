5ba13500711bf9b24da1010f53021eb1bbe8c438
Dragonmystic <dragonmystic707@gmail.com>
2020-10-26 21:36:56 -0500

    Reset Script to default

--- docs/public/js/script.js
+++ docs/public/js/script.js
@@ -2,10 +2,7 @@
   const content = document.querySelector('.page');
 
   scrollnav.init(content, { 
-    debug: false,
-    easingStyle: 'linear',
-    sections: ($('.page > h1').length>0) ? 'h1' : 'h2',
-    subSections: ($('.page > h1').length>0) ? 'h2' : 'h3'
+    debug: false
   });
 })(document);
 
