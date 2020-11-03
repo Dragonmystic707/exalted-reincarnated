24b32082a8985094227e30fb16700fa731a13d13
Dragonmystic <dragonmystic707@gmail.com>
2020-11-02 22:04:34 -0600

    Scrollnav test

--- docs/public/js/script.js
+++ docs/public/js/script.js
@@ -1,15 +1,22 @@
+const scrollnavMin = require("./scrollnav/scrollnav.min");
+
 (function(document) {
   const content = document.querySelector('.page');
   var activeSidebarItem = document.querySelector('.sidebar-nav > a.active');
 
-  $('.page').scrollNav({
-    sections: 'h1',
-    subSections: 'h2',
+  // $('.page').scrollNav({
+  //   sections: 'h1',
+  //   subSections: 'h2',
+  //   insertTarget: activeSidebarItem,
+  //   insertLocation: 'insertAfter',
+  //   headlineText: '',
+  //   showTopLink: false,
+  // })
+
+  scrollnav.init(content, {
     insertTarget: activeSidebarItem,
-    insertLocation: 'insertAfter',
-    headlineText: '',
-    showTopLink: false,
-  })
+    insertLocation: 'after'
+  });
 
 })(document);
 
