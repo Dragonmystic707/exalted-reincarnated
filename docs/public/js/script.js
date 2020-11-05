f3eca1abd940fac9624c65c0ce510fb63ba64c58
Dragonmystic <dragonmystic707@gmail.com>
2020-11-04 22:51:31 -0600

    Added collapsible Sidebars

--- docs/public/js/script.js
+++ docs/public/js/script.js
@@ -16,6 +16,29 @@
     insertLocation: 'after'
   });
 
+  var coll = document.getElementsByClassName("collapsible");
+  var i;
+
+  const checkHeight = function(obj, expanded) {
+    if (expanded){
+      obj.style.maxHeight = obj.scrollHeight + "px";
+    } else {
+      obj.style.maxHeight = null;    
+    }
+  }
+  /* Temporarily disable the transition duration */
+  
+
+  for (i = 0; i < coll.length; i++) {
+    checkHeight(coll[i].nextElementSibling, coll[i].classList.contains("active"));
+    
+    coll[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var content = this.nextElementSibling;
+      checkHeight(content, this.classList.contains("active"));
+    });
+  }
 })(document);
+
 
 
